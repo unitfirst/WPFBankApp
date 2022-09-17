c0ebd0bb246370c4da4493ee11bcef30f50a8926
Mattthew Prokofev <mattthew.pr@gmail.com>
2022-09-17 19:49:49 +0300

    -add View

---
+++ WPFBankApp/General/MVVM/View/MainWindowView/Views/AccountsView.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+using WPFBankApp.General.MVVM.ViewModel.MainWindowVM;
+
+namespace WPFBankApp.General.MVVM.View.MainWindowView.Views;
+
+public partial class AccountsView : UserControl
+{
+    public AccountsView()
+    {
+        InitializeComponent();
+        DataContext = new AccountsViewModel();
+    }
+}
