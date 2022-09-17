f958cfa7eafd998ea7b7fc8a3952f6658ca01bfe
Mattthew Prokofev <mattthew.pr@gmail.com>
2022-09-17 20:04:12 +0300

    -fix Passport and PhoneNumber binding

---
+++ WPFBankApp/General/Core/Converters/PhoneNumberToString.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using WPFBankApp.General.MVVM.Model.Accounts.ProtectedData;
+
+namespace WPFBankApp.General.Core.Converters;
+
+public class PhoneNumberToString : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (!(value is PhoneNumber data)) return null;
+
+        return data.ToString();
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
