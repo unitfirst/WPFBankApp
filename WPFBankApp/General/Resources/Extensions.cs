da2b7a349719c183bdce99f3b707ab9bd057fdf3
Mattthew Prokofev <mattthew.pr@gmail.com>
2022-09-17 19:36:21 +0300

    -add Extensions and icons

---
+++ WPFBankApp/General/Resources/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace WPFBankApp.General.Resources;
+
+internal class Extensions
+{
+    public static readonly DependencyProperty Icon = 
+        DependencyProperty.RegisterAttached
+        (
+            "Icon", 
+            typeof(string), 
+            typeof(Extensions), 
+            new PropertyMetadata(default(string)
+            )
+        );
+
+    public static void SetIcon(UIElement element, string value)
+    {
+        element.SetValue(Icon, value);
+    }
+
+    public static string GetIcon(UIElement element)
+    {
+        return (string)element.GetValue(Icon);
+    }
+}
