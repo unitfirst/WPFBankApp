788207ebfd9b2b2e952441d64312795f9ae7b4b4
Mattthew Prokofev <mattthew.pr@gmail.com>
2022-09-18 14:23:07 +0300

    -add Bank

---
+++ WPFBankApp/General/Service/Bank.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using WPFBankApp.General.Data.Interface;
+using WPFBankApp.General.MVVM.Model.Accounts;
+using WPFBankApp.General.MVVM.Model.Employees.Base;
+
+namespace WPFBankApp.General.Service;
+
+public class Bank
+{
+    private readonly Employee _employee;
+
+    private string _name;
+
+    public Bank(string name, IRepository repository, Employee employee)
+    {
+        _name = name;
+        _employee = employee;
+    }
+
+    public IRepository Repository { get; set; }
+
+    public IEnumerable<AccountInfo> GetAccountsInfo()
+    {
+        var list = new List<AccountInfo>();
+        foreach (var item in Repository) list.Add(_employee.GetAccountInfo(item));
+
+        return list;
+    }
+}
