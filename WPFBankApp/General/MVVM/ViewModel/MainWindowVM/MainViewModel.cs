5fe07e6f37c5692a7255978e8b24d0af792cbe70
Mattthew Prokofev <mattthew.pr@gmail.com>
2022-09-18 15:01:01 +0300

    -reformat code

--- WPFBankApp/General/MVVM/ViewModel/MainWindowVM/MainViewModel.cs
+++ WPFBankApp/General/MVVM/ViewModel/MainWindowVM/MainViewModel.cs
@@ -6,106 +6,128 @@ using WPFBankApp.General.MVVM.ViewModel.Base;
 
 namespace WPFBankApp.General.MVVM.ViewModel.MainWindowVM;
 
-    public class MainViewModel : ViewModelBase
+public class MainViewModel : ViewModelBase
+{
+    public MainViewModel()
     {
-        #region Current View
+    }
 
-        private object _currentView;
-        public object CurrentView
+    public MainViewModel(Employee employee)
+    {
+        #region Views
+
+        AccountsVm = new AccountsViewModel();
+        NewAccountVm = new NewAccountViewModel();
+        SettingsVm = new SettingsViewModel();
+        AboutVm = new AboutViewModel();
+
+        CurrentView = AccountsVm;
+        Employee = employee;
+
+        #endregion
+
+        #region Change view commands
+
+        ShowAccountsView = new RelayCommand(OnShowAccountsViewExecuted, CanShowAccountsViewExecute);
+        ShowSettingsView = new RelayCommand(OnShowSettingsViewExecuted, CanShowSettingsViewExecute);
+        ShowNewAccountView = new RelayCommand(OnShowNewAccountViewExecuted, CanShowNewAccountViewExecute);
+        ShowAboutView = new RelayCommand(OnShowAboutViewExecuted, CanShowAboutViewExecute);
+
+        #endregion
+    }
+
+    #region Current View
+
+    private object _currentView;
+
+    public object CurrentView
+    {
+        get => _currentView;
+        set
         {
-            get { return _currentView; }
-            set
-            {
-                _currentView = value;
-                OnPropertyChanged();
-            }
-        }
-
-
-        #endregion
-
-        #region View fields
-
-        private AccountsViewModel AccountsVm { get; set; }
-        private SettingsViewModel SettingsVm { get; set; }
-        private NewAccountViewModel NewAccountVm { get; set; }
-        private AboutViewModel AboutVm { get; set; }
-        public Account Account { get; set; }
-        public Employee Employee { get; set; }
-
-
-        #endregion
-
-        #region Commands
-
-        #region ShowAccountsView
-
-        public ICommand ShowAccountsView { get; }
-        private bool CanShowAccountsViewExecute(object p) => true;
-        private void OnShowAccountsViewExecuted(object p)
-        {
-            CurrentView = AccountsVm;
-        }
-
-        #endregion
-
-        #region ShowSettingsView
-
-        public ICommand ShowSettingsView { get; }
-        private bool CanShowSettingsViewExecute(object p) => true;
-        private void OnShowSettingsViewExecuted(object p)
-        {
-            CurrentView = SettingsVm;
-        }
-
-        #endregion
-
-        #region ShowSettingsView
-
-        public ICommand ShowNewAccountView { get; }
-        private bool CanShowNewAccountViewExecute(object p) => true;
-        private void OnShowNewAccountViewExecuted(object p)
-        {
-            CurrentView = NewAccountVm;
-        }
-
-        #endregion
-
-        #region ShowAboutView
-
-        public ICommand ShowAboutView { get; }
-        private bool CanShowAboutViewExecute(object p) => true;
-        private void OnShowAboutViewExecuted(object p)
-        {
-            CurrentView = AboutVm;
-        }
-
-        #endregion
-
-        #endregion
-
-        public MainViewModel() { }
-        public MainViewModel(Employee employee)
-        {
-            #region Views
-
-            AccountsVm = new AccountsViewModel();
-            NewAccountVm = new NewAccountViewModel();
-            SettingsVm = new SettingsViewModel();
-            AboutVm = new AboutViewModel();
-
-            CurrentView = AccountsVm;
-            Employee = employee;
-
-            #endregion
-
-            #region Change view commands
-
-            ShowAccountsView = new RelayCommand(OnShowAccountsViewExecuted, CanShowAccountsViewExecute);
-            ShowSettingsView = new RelayCommand(OnShowSettingsViewExecuted, CanShowSettingsViewExecute);
-            ShowNewAccountView = new RelayCommand(OnShowNewAccountViewExecuted, CanShowNewAccountViewExecute);
-            ShowAboutView = new RelayCommand(OnShowAboutViewExecuted, CanShowAboutViewExecute);
-
-            #endregion
+            _currentView = value;
+            OnPropertyChanged();
         }
     }
+
+    #endregion
+
+    #region View fields
+
+    private AccountsViewModel AccountsVm { get; }
+    private SettingsViewModel SettingsVm { get; }
+    private NewAccountViewModel NewAccountVm { get; }
+    private AboutViewModel AboutVm { get; }
+    public Account Account { get; set; }
+    public Employee Employee { get; set; }
+
+    #endregion
+
+    #region Commands
+
+    #region ShowAccountsView
+
+    public ICommand ShowAccountsView { get; }
+
+    private bool CanShowAccountsViewExecute(object p)
+    {
+        return true;
+    }
+
+    private void OnShowAccountsViewExecuted(object p)
+    {
+        CurrentView = AccountsVm;
+    }
+
+    #endregion
+
+    #region ShowSettingsView
+
+    public ICommand ShowSettingsView { get; }
+
+    private bool CanShowSettingsViewExecute(object p)
+    {
+        return true;
+    }
+
+    private void OnShowSettingsViewExecuted(object p)
+    {
+        CurrentView = SettingsVm;
+    }
+
+    #endregion
+
+    #region ShowSettingsView
+
+    public ICommand ShowNewAccountView { get; }
+
+    private bool CanShowNewAccountViewExecute(object p)
+    {
+        return true;
+    }
+
+    private void OnShowNewAccountViewExecuted(object p)
+    {
+        CurrentView = NewAccountVm;
+    }
+
+    #endregion
+
+    #region ShowAboutView
+
+    public ICommand ShowAboutView { get; }
+
+    private bool CanShowAboutViewExecute(object p)
+    {
+        return true;
+    }
+
+    private void OnShowAboutViewExecuted(object p)
+    {
+        CurrentView = AboutVm;
+    }
+
+    #endregion
+
+    #endregion
+}
