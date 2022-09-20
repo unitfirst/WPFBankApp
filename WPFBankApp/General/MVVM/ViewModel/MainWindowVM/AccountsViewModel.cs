003cfa8d19070ebf1c83cbe1f30193dd5e185e8f
Mattthew Prokofev <mattthew.pr@gmail.com>
2022-09-20 19:41:45 +0300

    -clean AccountsView

--- WPFBankApp/General/MVVM/ViewModel/MainWindowVM/AccountsViewModel.cs
+++ WPFBankApp/General/MVVM/ViewModel/MainWindowVM/AccountsViewModel.cs
@@ -1,28 +1,40 @@
 ﻿using System;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
 using System.Windows.Input;
 using WPFBankApp.AppFiles.Core.Commands;
 using WPFBankApp.General.MVVM.Model.Accounts;
-using WPFBankApp.General.MVVM.Model.Accounts.ProtectedData;
 using WPFBankApp.General.MVVM.ViewModel.Base;
 
 namespace WPFBankApp.General.MVVM.ViewModel.MainWindowVM;
 
 public class AccountsViewModel : ViewModelBase
 {
-    public Action UpdateAccountsList;
-    public ObservableCollection<Account> Accounts { get; set; }
     public MainViewModel MainVm;
-    
-    public AccountsViewModel(){}
+    public Action UpdateAccountsList;
+
+    public AccountsViewModel()
+    {
+    }
+
     public AccountsViewModel(MainViewModel mainVm)
     {
         MainVm = mainVm;
         Accounts = MainVm.Accounts;
 
-        RemoveAccountCommand = new RelayCommand(OnRemoveAccountExecuted,CanRemoveAccountExecute);
-        UpdateAccountCommand = new RelayCommand(OnUpdateAccountCommandExecuted,CanUpdateAccountCommandExecute);
+        RemoveAccountCommand = new RelayCommand(OnRemoveAccountExecuted, CanRemoveAccountExecute);
+        UpdateAccountCommand = new RelayCommand(OnUpdateAccountCommandExecuted, CanUpdateAccountCommandExecute);
+    }
+
+    public ObservableCollection<Account> Accounts { get; set; }
+
+    public void UpdateList()
+    {
+        Accounts.Clear();
+        foreach (var item in MainVm.Bank.GetAccountsInfo()) Accounts.Add(item);
+
+        SelectedIndex = _selectedIndex;
+
+        //EnableEditClient = MainVm.Employee.DataAccess.Commands.EditClient && Clients.Count > 0;
     }
 
     #region Selected
@@ -64,22 +76,28 @@ public class AccountsViewModel : ViewModelBase
     #region Commands
 
     #region RemoveAccountCommand
+
     public ICommand RemoveAccountCommand { get; }
 
     private void OnRemoveAccountExecuted(object p)
     {
-        if(SelectedAccount is null) return;
-        
+        if (SelectedAccount is null) return;
+
         MainVm.Bank.RemoveAccount(SelectedAccount);
         UpdateList();
     }
 
-    private bool CanRemoveAccountExecute(object p) => true;
+    private bool CanRemoveAccountExecute(object p)
+    {
+        return true;
+    }
+
     #endregion
 
     #region UpdateAccountCommand
-    
+
     public ICommand UpdateAccountCommand { get; }
+
     private void OnUpdateAccountCommandExecuted(object p)
     {
         Account tempAccount = SelectedAccount;
@@ -88,26 +106,16 @@ public class AccountsViewModel : ViewModelBase
         Accounts.Remove(SelectedAccount);
         Accounts.Insert(index, tempAccount);
         SelectedAccount = (AccountInfo)tempAccount;
-        
+
         UpdateList();
-
     }
-    private bool CanUpdateAccountCommandExecute(object p) => true;
 
-    #endregion
-
-    #endregion
-
-    public void UpdateList()
+    private bool CanUpdateAccountCommandExecute(object p)
     {
-        Accounts.Clear();
-        foreach (var item in MainVm.Bank.GetAccountsInfo())
-        {
-            Accounts.Add(item);
-        }
-
-        SelectedIndex = _selectedIndex;
-
-        //EnableEditClient = MainVm.Employee.DataAccess.Commands.EditClient && Clients.Count > 0;
+        return true;
     }
+
+    #endregion
+
+    #endregion
 }
